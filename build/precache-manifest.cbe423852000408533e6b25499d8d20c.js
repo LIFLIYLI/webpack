4cb52d942268ea5c1041959138394d0a592827a4
LIFLIYLI <806820876@qq.com>
2018-10-14 15:55:10 +0800

    build

---
+++ build/precache-manifest.cbe423852000408533e6b25499d8d20c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae5bb4b6e2ad21017e4b",
+    "url": "/static/js/main.ae5bb4b6.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "ae5bb4b6e2ad21017e4b",
+    "url": "/static/css/main.59572313.chunk.css"
+  },
+  {
+    "revision": "85bfdd2f2e3f533169e0b3524ee32863",
+    "url": "/index.html"
+  }
+];
